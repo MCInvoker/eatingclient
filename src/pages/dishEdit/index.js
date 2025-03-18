5a34826893faa908c6cadf56d99f4ad6420debd9
invoker <759302142@qq.com>
2025-03-18 20:02:10 +0800

    feat: 上传的菜肴图片可以编辑

--- src/pages/dishEdit/index.js
+++ src/pages/dishEdit/index.js
@@ -392,6 +392,7 @@ const DishEdit = () => {
                 count: 9, // 最多可以选择的图片数量
                 sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
                 sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+                edit: true // 开启编辑功能
             });
 
             // 使用Promise.all并行获取所有图片信息
